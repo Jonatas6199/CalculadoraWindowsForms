9c3b9fd0d67b2d8fc76e38536f0605f27c3140eb
jonatas-joao <jojokl@hotmail.com>
2025-03-21 22:11:30 -0300

    Add missing operations

--- CalculadoraWindowsForms/Form1.Designer.cs
+++ CalculadoraWindowsForms/Form1.Designer.cs
@@ -40,9 +40,9 @@
             button9 = new Button();
             button10 = new Button();
             btnSomar = new Button();
-            button12 = new Button();
-            button13 = new Button();
-            button14 = new Button();
+            btnSubtrair = new Button();
+            btnMultiplicar = new Button();
+            btnDividir = new Button();
             btnResultado = new Button();
             button1 = new Button();
             SuspendLayout();
@@ -50,6 +50,7 @@
             // txtOperacao
             // 
             txtOperacao.Enabled = false;
+            txtOperacao.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             txtOperacao.Location = new Point(20, 33);
             txtOperacao.Multiline = true;
             txtOperacao.Name = "txtOperacao";
@@ -84,6 +85,7 @@
             btn3.TabIndex = 3;
             btn3.Text = "3";
             btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
             // 
             // btn4
             // 
@@ -93,6 +95,7 @@
             btn4.TabIndex = 4;
             btn4.Text = "4";
             btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
             // 
             // btn5
             // 
@@ -102,6 +105,7 @@
             btn5.TabIndex = 5;
             btn5.Text = "5";
             btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
             // 
             // button6
             // 
@@ -111,6 +115,7 @@
             button6.TabIndex = 6;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += btn6_Click;
             // 
             // button7
             // 
@@ -120,6 +125,7 @@
             button7.TabIndex = 7;
             button7.Text = "7";
             button7.UseVisualStyleBackColor = true;
+            button7.Click += btn7_Click;
             // 
             // button8
             // 
@@ -129,6 +135,7 @@
             button8.TabIndex = 8;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += btn8_Click;
             // 
             // button9
             // 
@@ -138,6 +145,7 @@
             button9.TabIndex = 9;
             button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += btn9_Click;
             // 
             // button10
             // 
@@ -147,6 +155,7 @@
             button10.TabIndex = 10;
             button10.Text = "0";
             button10.UseVisualStyleBackColor = true;
+            button10.Click += btn0_Click;
             // 
             // btnSomar
             // 
@@ -158,32 +167,35 @@
             btnSomar.UseVisualStyleBackColor = true;
             btnSomar.Click += btnSomar_Click;
             // 
-            // button12
+            // btnSubtrair
             // 
-            button12.Location = new Point(267, 99);
-            button12.Name = "button12";
-            button12.Size = new Size(53, 52);
-            button12.TabIndex = 12;
-            button12.Text = "-";
-            button12.UseVisualStyleBackColor = true;
+            btnSubtrair.Location = new Point(267, 99);
+            btnSubtrair.Name = "btnSubtrair";
+            btnSubtrair.Size = new Size(53, 52);
+            btnSubtrair.TabIndex = 12;
+            btnSubtrair.Text = "-";
+            btnSubtrair.UseVisualStyleBackColor = true;
+            btnSubtrair.Click += btnSubtrair_Click;
             // 
-            // button13
+            // btnMultiplicar
             // 
-            button13.Location = new Point(267, 170);
-            button13.Name = "button13";
-            button13.Size = new Size(53, 52);
-            button13.TabIndex = 13;
-            button13.Text = "*";
-            button13.UseVisualStyleBackColor = true;
+            btnMultiplicar.Location = new Point(267, 170);
+            btnMultiplicar.Name = "btnMultiplicar";
+            btnMultiplicar.Size = new Size(53, 52);
+            btnMultiplicar.TabIndex = 13;
+            btnMultiplicar.Text = "*";
+            btnMultiplicar.UseVisualStyleBackColor = true;
+            btnMultiplicar.Click += btnMultiplicar_Click;
             // 
-            // button14
+            // btnDividir
             // 
-            button14.Location = new Point(267, 242);
-            button14.Name = "button14";
-            button14.Size = new Size(53, 52);
-            button14.TabIndex = 14;
-            button14.Text = "/";
-            button14.UseVisualStyleBackColor = true;
+            btnDividir.Location = new Point(267, 242);
+            btnDividir.Name = "btnDividir";
+            btnDividir.Size = new Size(53, 52);
+            btnDividir.TabIndex = 14;
+            btnDividir.Text = "/";
+            btnDividir.UseVisualStyleBackColor = true;
+            btnDividir.Click += btnDividir_Click;
             // 
             // btnResultado
             // 
@@ -203,6 +215,7 @@
             button1.TabIndex = 16;
             button1.Text = "Limpar";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += btnLimpar_Click;
             // 
             // Form1
             // 
@@ -211,9 +224,9 @@
             ClientSize = new Size(348, 409);
             Controls.Add(button1);
             Controls.Add(btnResultado);
-            Controls.Add(button14);
-            Controls.Add(button13);
-            Controls.Add(button12);
+            Controls.Add(btnDividir);
+            Controls.Add(btnMultiplicar);
+            Controls.Add(btnSubtrair);
             Controls.Add(btnSomar);
             Controls.Add(button10);
             Controls.Add(button9);
@@ -246,9 +259,9 @@
         private Button button9;
         private Button button10;
         private Button btnSomar;
-        private Button button12;
-        private Button button13;
-        private Button button14;
+        private Button btnSubtrair;
+        private Button btnMultiplicar;
+        private Button btnDividir;
         private Button btnResultado;
         private Button button1;
     }
--- CalculadoraWindowsForms/Form1.cs
+++ CalculadoraWindowsForms/Form1.cs
@@ -11,14 +11,17 @@ namespace CalculadoraWindowsForms
         {
             InitializeComponent();
         }
-
-        private void btn1_Click(object sender, EventArgs e)
+        private bool ExisteOperacao()
         {
-            txtOperacao.Text += "1";
+            if(dividir || somar || multiplicar || subtrair)
+                return true;
+            return false;
         }
 
         private void btnSomar_Click(object sender, EventArgs e)
         {
+            if (txtOperacao.Text.Trim() == string.Empty || ExisteOperacao())
+                return;
             dividir = false;
             subtrair = false;
             multiplicar = false;
@@ -35,8 +38,72 @@ namespace CalculadoraWindowsForms
             txtOperacao.Text += "+";
         }
 
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            if (txtOperacao.Text.Trim() == string.Empty || ExisteOperacao())
+                return;
+
+            dividir = false;
+            subtrair = true;
+            multiplicar = false;
+            somar = false;
+
+            if (txtOperacao.Text[txtOperacao.Text.Length - 1] == '+'
+                || txtOperacao.Text[txtOperacao.Text.Length - 1] == '*'
+                || txtOperacao.Text[txtOperacao.Text.Length - 1] == '/')
+            {
+                txtOperacao.Text = txtOperacao.Text.Remove(txtOperacao.Text.Length - 1);
+            }
+            if (txtOperacao.Text[txtOperacao.Text.Length - 1] == '-')
+                return;
+            txtOperacao.Text += "-";
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            if (txtOperacao.Text.Trim() == string.Empty || ExisteOperacao())
+                return;
+            dividir = false;
+            subtrair = false;
+            multiplicar = true;
+            somar = false;
+
+            if (txtOperacao.Text[txtOperacao.Text.Length - 1] == '+'
+                || txtOperacao.Text[txtOperacao.Text.Length - 1] == '-'
+                || txtOperacao.Text[txtOperacao.Text.Length - 1] == '/')
+            {
+                txtOperacao.Text = txtOperacao.Text.Remove(txtOperacao.Text.Length - 1);
+            }
+            if (txtOperacao.Text[txtOperacao.Text.Length - 1] == '*')
+                return;
+            txtOperacao.Text += "*";
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            if (txtOperacao.Text.Trim() == string.Empty || ExisteOperacao())
+                return;
+
+            dividir = true;
+            subtrair = false;
+            multiplicar = false;
+            somar = false;
+
+            if (txtOperacao.Text[txtOperacao.Text.Length - 1] == '+'
+                || txtOperacao.Text[txtOperacao.Text.Length - 1] == '-'
+                || txtOperacao.Text[txtOperacao.Text.Length - 1] == '*')
+            {
+                txtOperacao.Text = txtOperacao.Text.Remove(txtOperacao.Text.Length - 1);
+            }
+            if (txtOperacao.Text[txtOperacao.Text.Length - 1] == '/')
+                return;
+            txtOperacao.Text += "/";
+        }
+
         private void btnResultado_Click(object sender, EventArgs e)
         {
+            if (!somar && !subtrair && !dividir && !multiplicar)
+                return;
             if (somar)
             {
                 List<string> aSomar = txtOperacao.Text.Split("+").ToList();
@@ -49,21 +116,81 @@ namespace CalculadoraWindowsForms
             }
             else if (subtrair)
             {
-                //Escrever o código
+                List<string> aSomar = txtOperacao.Text.Split("-").ToList();
+
+                double primeiroNumero = Convert.ToDouble(aSomar[0]);
+                double segundoNumero = Convert.ToDouble(aSomar[1]);
+
+                double resultado = primeiroNumero - segundoNumero;
+                txtOperacao.Text = resultado.ToString();
             }
             else if (dividir)
             {
-                //Escrever o código
+                List<string> aSomar = txtOperacao.Text.Split("/").ToList();
+
+                double primeiroNumero = Convert.ToDouble(aSomar[0]);
+                double segundoNumero = Convert.ToDouble(aSomar[1]);
+
+                double resultado = primeiroNumero / segundoNumero;
+                txtOperacao.Text = resultado.ToString();
             }
             else if (multiplicar)
             {
-                //Escrever o código    
-            }
-        }
+                List<string> aSomar = txtOperacao.Text.Split("*").ToList();
 
+                double primeiroNumero = Convert.ToDouble(aSomar[0]);
+                double segundoNumero = Convert.ToDouble(aSomar[1]);
+
+                double resultado = primeiroNumero * segundoNumero;
+                txtOperacao.Text = resultado.ToString();
+            }
+            somar = dividir = multiplicar = subtrair = false;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "1";
+        }
         private void btn2_Click(object sender, EventArgs e)
         {
             txtOperacao.Text += "2";
         }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "3";
+        }
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "4";
+        }
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "5";
+        }
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "6";
+        }
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "7";
+        }
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "8";
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "9";
+        }
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text += "0";
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtOperacao.Text = string.Empty;
+        }
+
+        
     }
 }
